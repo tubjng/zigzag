5982a18c90d0e9d3a1efcba5c6b55b507a424aa9
tubjng <mac@macs-MacBook-Pro.local>
2015-11-24 09:54:54 +0700

    1st commit

--- zigzag/ViewController.m
+++ zigzag/ViewController.m
@@ -16,12 +16,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self tamgiac];
+}
+-(void)tamgiac{
+    int n, i, j;
+    
+    printf("\nNhap chieu cao cua hinh tam giac : ");
+    scanf("%d", &n);
+    
+    for (i=0; i<n; i++)
+    {
+        for (j=n-1; j>i; j--)
+            printf(" ");
+        for (j=0; j<i*2+1; j++)
+            printf("*");
+        printf("\n");
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+   
 }
 
 @end
